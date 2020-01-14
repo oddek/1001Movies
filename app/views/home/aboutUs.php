1864262d44c442626250c78197743f77fbcf0784
Kent Odde <kentodde89@gmail.com>
2020-01-14 20:29:12 +0100

    changed the text in about

--- app/views/home/aboutUs.php
+++ app/views/home/aboutUs.php
@@ -9,7 +9,7 @@ include VIEW . 'header.php';
     <div class="col-md-10 col-lg-8">
       <br>
      <h1 class="h4">About</h1>
-     <p class="mt-4 mb-4">We've tried to make your journey through Steven Schneider's book a bit more structured. Tag the movies you have seen, and feel the dopamine rush as the progress bar turns green! </p>
+     <p class="mt-4 mb-4">We've tried to make your journey through Steven Schneider's book a bit more structured. Tag the movies you have seen, and feel the dopamine rush as the progress bar fills up! </p>
     </div>
    </div>
 
